d19af1811ed1038b31e76a9943c2bdc1055e5051
YoonPro <krafftdj@daum.net>
2022-11-12 22:09:20 +0900

    logback-spring.xml 추가
    log4 jdbc 설정 완료
    화면에 데이터가 있을때 콘솔에 어떤 쿼리를 썼는지
    시간은 얼마나 걸리는지 처리되는 모습을 볼 수 있다.

--- YOON_SpringBoot/src/main/java/com/yoon/YoonSpringBootApplication.java
+++ YOON_SpringBoot/src/main/java/com/yoon/YoonSpringBootApplication.java
@@ -4,21 +4,17 @@ package com.yoon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// log4j2 설정
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 
 @SpringBootApplication
 public class YoonSpringBootApplication {
 
-	private static Logger logger = LogManager.getLogger(YoonSpringBootApplication.class);
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(YoonSpringBootApplication.class, args);
 		
-		//logger.info("Staring Spring Boot application..");
-		//SpringApplication app = new SpringApplication(YoonSpringBootApplication.class);
-		//app.run(args);
+		
 	}
 
 }
